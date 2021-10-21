2071a7d3c5876af0f21ec6264d676bc4e089e201
Li Smith <jinrishouji@sina.com>
2021-10-21 13:13:14 +0800

    fix inspect build error

--- inspect/src/command.rs
+++ inspect/src/command.rs
@@ -21,37 +21,44 @@
 use crate::cli::{InspectCmd, InspectSubCmd};
 use crate::Inspector;
 use sc_cli::{CliConfiguration, ImportParams, Result, SharedParams};
+use sc_executor::NativeElseWasmExecutor;
 use sc_service::{new_full_client, Configuration, NativeExecutionDispatch};
 use sp_runtime::traits::Block;
 use std::str::FromStr;
 
 impl InspectCmd {
-    /// Run the inspect command, passing the inspector.
-    pub fn run<B, RA, EX>(&self, config: Configuration) -> Result<()>
-    where
-        B: Block,
-        B::Hash: FromStr,
-        RA: Send + Sync + 'static,
-        EX: NativeExecutionDispatch + 'static,
-    {
-        let client = new_full_client::<B, RA, EX>(&config)?;
-        let inspect = Inspector::<B>::new(client);
+	/// Run the inspect command, passing the inspector.
+	pub fn run<B, RA, EX>(&self, config: Configuration) -> Result<()>
+	where
+		B: Block,
+		B::Hash: FromStr,
+		RA: Send + Sync + 'static,
+		EX: NativeExecutionDispatch + 'static,
+	{
+		let executor = NativeElseWasmExecutor::<EX>::new(
+			config.wasm_method,
+			config.default_heap_pages,
+			config.max_runtime_instances,
+		);
 
-        match &self.command {
-            InspectSubCmd::Block { input } => {
-                let input = input.parse()?;
-                let res = inspect.block(input).map_err(|e| format!("{}", e))?;
-                println!("{}", res);
-                Ok(())
-            }
-            InspectSubCmd::Extrinsic { input } => {
-                let input = input.parse()?;
-                let res = inspect.extrinsic(input).map_err(|e| format!("{}", e))?;
-                println!("{}", res);
-                Ok(())
-            }
-        }
-    }
+		let client = new_full_client::<B, RA, _>(&config, None, executor)?;
+		let inspect = Inspector::<B>::new(client);
+
+		match &self.command {
+			InspectSubCmd::Block { input } => {
+				let input = input.parse()?;
+				let res = inspect.block(input).map_err(|e| format!("{}", e))?;
+				println!("{}", res);
+				Ok(())
+			},
+			InspectSubCmd::Extrinsic { input } => {
+				let input = input.parse()?;
+				let res = inspect.extrinsic(input).map_err(|e| format!("{}", e))?;
+				println!("{}", res);
+				Ok(())
+			},
+		}
+	}
 }
 
 impl CliConfiguration for InspectCmd {
