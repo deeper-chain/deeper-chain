e8261c769cc72082b7b6ebccffc37c65ab74de37
github-action-benchmark <github@users.noreply.github.com>
2022-01-06 02:10:57 +0000

    add Deeper Chain Benchmark (cargo) benchmark result for 4bad72282fe11b7beec2c36799ec901ef406285e

---
+++ dev/bench/data.js
@@ -0,0 +1,40 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1641435057243,
+  "repoUrl": "https://github.com/deeper-chain/deeper-chain",
+  "entries": {
+    "Deeper Chain Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "deeper-chain",
+            "username": "deeper-chain"
+          },
+          "committer": {
+            "name": "deeper-chain",
+            "username": "deeper-chain"
+          },
+          "id": "4bad72282fe11b7beec2c36799ec901ef406285e",
+          "message": "fix bench error",
+          "timestamp": "2022-01-05T01:19:07Z",
+          "url": "https://github.com/deeper-chain/deeper-chain/pull/158/commits/4bad72282fe11b7beec2c36799ec901ef406285e"
+        },
+        "date": 1641435056653,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "execute blocks/native",
+            "value": 4287835,
+            "range": "± 967314",
+            "unit": "ns/iter"
+          },
+          {
+            "name": "execute blocks/Wasm(Interpreted)",
+            "value": 81747980,
+            "range": "± 16546094",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
