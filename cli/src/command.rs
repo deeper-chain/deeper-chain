83ac762ebf4588d12968ffca92407d96de9a6ef9
Joey <joey.xie@deeper.network>
2022-01-07 11:10:46 +0800

    remove light client

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -79,11 +79,7 @@ pub fn run() -> Result<()> {
         None => {
             let runner = cli.create_runner(&cli.run)?;
             runner.run_node_until_exit(|config| async move {
-                match config.role {
-                    Role::Light => service::new_light(config),
-                    _ => service::new_full(config),
-                }
-                .map_err(sc_cli::Error::Service)
+                service::new_full(config).map_err(sc_cli::Error::Service)
             })
         }
         Some(Subcommand::Inspect(cmd)) => {
