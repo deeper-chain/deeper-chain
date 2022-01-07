555b4f209eb1ec3ac19087d656967d9cb8dec8bc
Joey <joey.xie@deeper.network>
2022-01-07 12:07:03 +0800

    remove light client

--- cli/src/lib.rs
+++ cli/src/lib.rs
@@ -16,33 +16,15 @@
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-//! Substrate CLI library.
-//!
-//! This package has two Cargo features:
-//!
-//! - `cli` (default): exposes functions that parse command-line options, then start and run the
-//! node as a CLI application.
-//!
-//! - `browser`: exposes the content of the `browser` module, which consists of exported symbols
-//! that are meant to be passed through the `wasm-bindgen` utility and called from JavaScript.
-//! Despite its name the produced WASM can theoretically also be used from NodeJS, although this
-//! hasn't been tested.
-
-#![warn(missing_docs)]
-
 pub mod chain_spec;
 
 #[macro_use]
 mod service;
-#[cfg(feature = "browser")]
-mod browser;
 #[cfg(feature = "cli")]
 mod cli;
 #[cfg(feature = "cli")]
 mod command;
 
-#[cfg(feature = "browser")]
-pub use browser::*;
 #[cfg(feature = "cli")]
 pub use cli::*;
 #[cfg(feature = "cli")]
