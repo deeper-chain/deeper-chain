d2b331659e8a89f716df6493e711547f82ec032d
Zhuang Jin <zjin@deeper.network>
2022-01-09 16:49:25 -0800

    add additional evm precompiles

--- runtime/src/precompiles.rs
+++ runtime/src/precompiles.rs
@@ -6,6 +6,12 @@ use pallet_evm_precompile_modexp::Modexp;
 use pallet_evm_precompile_sha3fips::Sha3FIPS256;
 use pallet_evm_precompile_simple::{ECRecover, ECRecoverPublicKey, Identity, Ripemd160, Sha256};
 
+use pallet_evm_precompile_blake2::Blake2F;
+use pallet_evm_precompile_bn128::{Bn128Add, Bn128Mul, Bn128Pairing};
+use pallet_evm_precompile_dispatch::Dispatch;
+use pallet_evm_precompile_curve25519::{Curve25519Add, Curve25519ScalarMul};
+use pallet_evm_precompile_ed25519::Ed25519Verify;
+
 pub struct FrontierPrecompiles<R>(PhantomData<R>);
 
 impl<R> FrontierPrecompiles<R>
@@ -24,6 +30,7 @@ where
 }
 impl<R> PrecompileSet for FrontierPrecompiles<R>
 where
+    Dispatch<R>: Precompile,
     R: pallet_evm::Config,
 {
     fn execute(
@@ -41,11 +48,27 @@ where
             a if a == hash(3) => Some(Ripemd160::execute(input, target_gas, context, is_static)),
             a if a == hash(4) => Some(Identity::execute(input, target_gas, context, is_static)),
             a if a == hash(5) => Some(Modexp::execute(input, target_gas, context, is_static)),
+            a if a == hash(6) => Some(Bn128Add::execute(input, target_gas, context, is_static)),
+            a if a == hash(7) => Some(Bn128Mul::execute(input, target_gas, context, is_static)),
+            a if a == hash(8) => Some(Bn128Pairing::execute(input, target_gas, context, is_static)),
+            a if a == hash(9) => Some(Blake2F::execute(input, target_gas, context, is_static)),
             // Non-Frontier specific nor Ethereum precompiles :
             a if a == hash(1024) => {
                 Some(Sha3FIPS256::execute(input, target_gas, context, is_static))
             }
             a if a == hash(1025) => Some(ECRecoverPublicKey::execute(
+                input, target_gas, context, is_static,
+            )),
+            a if a == hash(1026) => Some(Dispatch::<R>::execute(
+                input, target_gas, context, is_static,
+            )),
+            a if a == hash(1027) => Some(Curve25519Add::execute(
+                input, target_gas, context, is_static,
+            )),
+            a if a == hash(1028) => Some(Curve25519ScalarMul::execute(
+                input, target_gas, context, is_static,
+            )),
+            a if a == hash(1029) => Some(Ed25519Verify::execute(
                 input, target_gas, context, is_static,
             )),
             _ => None,
