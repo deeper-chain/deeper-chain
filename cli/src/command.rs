2d273ac55e45e57ac7c865b41eb3d592fe30da64
Joey <joey.xie@deeper.network>
2022-03-07 22:52:00 +0800

    remove useless staging and flamingfir config (#198)

--- cli/src/command.rs
+++ cli/src/command.rs
@@ -61,8 +61,6 @@ impl SubstrateCli for Cli {
             }
             "dev" => Box::new(chain_spec::development_config()),
             "local" => Box::new(chain_spec::local_testnet_config()),
-            "fir" | "flaming-fir" => Box::new(chain_spec::flaming_fir_config()?),
-            "staging" => Box::new(chain_spec::staging_testnet_config()),
             path => Box::new(chain_spec::ChainSpec::from_json_file(
                 std::path::PathBuf::from(path),
             )?),
