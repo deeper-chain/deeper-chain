25a77e64a0d863e3ebeedde2f82e6c97d2942094
AmadeusGB <1069830494@qq.com>
2022-03-08 09:43:15 +0800

    update spec_version:11

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -155,10 +155,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 10,
+    spec_version: 11,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 4,
+    transaction_version: 5,
 };
 
 /// The BABE epoch configuration at genesis.
