cdd49d3e20da49761a55ee626b2b48ec15f7813e
Joey <joey.xie@deeper.network>
2022-03-30 14:52:27 +0800

    expose runtime so other project can use (#215)
    * expose service and runtime
    
    * update node-bench and remove structopt dependency
    
    * remove unused criterion dependency

--- bench/src/tempdb.rs
+++ bench/src/tempdb.rs
@@ -1,6 +1,6 @@
 // This file is part of Substrate.
 
-// Copyright (C) 2020-2021 Parity Technologies (UK) Ltd.
+// Copyright (C) 2020-2022 Parity Technologies (UK) Ltd.
 // SPDX-License-Identifier: GPL-3.0-or-later WITH Classpath-exception-2.0
 
 // This program is free software: you can redistribute it and/or modify
@@ -104,7 +104,7 @@ impl TempDatabase {
                 column_options.ref_counted = true;
                 column_options.preimage = true;
                 column_options.uniform = true;
-                parity_db::Db::open(&options).expect("db open error")
+                parity_db::Db::open_or_create(&options).expect("db open error")
             })),
         }
     }
