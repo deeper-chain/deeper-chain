0fc1acb11a98d78d30d6e5455138a71191994e78
Li Smith <jinrishouji@sina.com>
2022-04-14 14:15:58 +0800

    fix other pallet mock.rs have no pallet_uniques

--- pallets/credit-accumulation/src/mock.rs
+++ pallets/credit-accumulation/src/mock.rs
@@ -16,7 +16,7 @@
 use crate as pallet_credit_accumulation;
 use crate::testing_utils::*;
 use frame_support::parameter_types;
-use frame_support::traits::ConstU32;
+use frame_support::traits::{ConstU128, ConstU32};
 use frame_system as system;
 use node_primitives::{Balance, Moment};
 use pallet_micropayment::AccountCreator;
@@ -42,6 +42,7 @@ frame_support::construct_runtime!(
         DeeperNode: pallet_deeper_node::{Pallet, Call, Storage, Event<T>, Config<T>},
         CreditAccumulation: pallet_credit_accumulation::{Pallet, Call, Storage, Event<T>},
         Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
+        Uniques: pallet_uniques::{Pallet, Call, Storage, Event<T>},
     }
 );
 
@@ -121,6 +122,34 @@ impl pallet_timestamp::Config for Test {
     type Moment = u64;
     type OnTimestampSet = ();
     type MinimumPeriod = MinimumPeriod;
+    type WeightInfo = ();
+}
+
+const MILLICENTS: Balance = 10_000_000_000_000;
+const CENTS: Balance = 1_000 * MILLICENTS;
+const DOLLARS: Balance = 100 * CENTS;
+parameter_types! {
+    pub const ClassDeposit: Balance = 100 * DOLLARS;
+    pub const InstanceDeposit: Balance = 1 * DOLLARS;
+    pub const KeyLimit: u32 = 32;
+    pub const ValueLimit: u32 = 256;
+    pub const StringLimit: u32 = 50;
+}
+
+impl pallet_uniques::Config for Test {
+    type Event = Event;
+    type ClassId = u32;
+    type InstanceId = u32;
+    type Currency = Balances;
+    type ForceOrigin = frame_system::EnsureRoot<AccountId>;
+    type ClassDeposit = ConstU128<2>;
+    type InstanceDeposit = ConstU128<1>;
+    type MetadataDepositBase = ConstU128<1>;
+    type AttributeDepositBase = ConstU128<1>;
+    type DepositPerByte = ConstU128<1>;
+    type StringLimit = ConstU32<50>;
+    type KeyLimit = ConstU32<50>;
+    type ValueLimit = ConstU32<50>;
     type WeightInfo = ();
 }
 
