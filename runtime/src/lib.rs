568e3295e8b81cd91e686b91752f2a0dc48c9394
AmadeusGB <1069830494@qq.com>
2022-06-10 22:34:48 +0800

    update spec_version:20 (#269)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -159,7 +159,7 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     spec_version: 20,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 9,
+    transaction_version: 10,
     state_version: 1,
 };
 
