6431dec1496652621f479cf689ef098cdd11ffa8
AmadeusGB <1069830494@qq.com>
2023-09-26 12:19:56 +0800

    restore NpowDayMintedDPR (#397)
    * restore NpowDayMintedDPR
    
    * modify reads_writes config

--- pallets/staking/src/lib.rs
+++ pallets/staking/src/lib.rs
@@ -2161,11 +2161,15 @@ pub mod pallet {
                     let delegator_payouts_per_block =
                         (Self::delegator_count() + paying_blocks_num - 1) / paying_blocks_num + 1;
 
+                    let era = T::CreditInterface::get_current_era();
+                    let default_dpr: BalanceOf<T> = Zero::zero();
+
                     DelegatorPayoutsPerBlock::<T>::put(delegator_payouts_per_block);
                     let prefix = Self::get_delegators_prefix_hash();
                     DelegatorsKeyPrefix::<T>::put(prefix.clone());
                     DelegatorsLastKey::<T>::put(prefix);
-                    weight = weight.saturating_add(T::DbWeight::get().reads_writes(3, 1));
+                    NpowDayMintedDPR::<T>::put((era, default_dpr));
+                    weight = weight.saturating_add(T::DbWeight::get().reads_writes(3, 4));
                 } else {
                     weight = weight.saturating_add(Self::pay_delegators());
                 }
