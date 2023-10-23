197e982ede07ae7a4a13342df9f54e1c2f32918b
yubo <anhuitianyu@163.com>
2023-10-23 11:24:55 +0800

    use official palllet-preimage (#410)

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1568,7 +1568,6 @@ pub type Executive = frame_executive::Executive<
     frame_system::ChainContext<Runtime>,
     Runtime,
     AllPalletsWithSystem,
-    pallet_preimage::migration::v1::Migration<Runtime>,
 >;
 
 impl fp_self_contained::SelfContainedCall for RuntimeCall {
