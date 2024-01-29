a4dc027248672faad3e81bafad11c1f958458295
AmadeusGB <1069830494@qq.com>
2024-01-29 16:54:54 +0800

    update spec_version:69

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -185,10 +185,10 @@ pub const VERSION: RuntimeVersion = RuntimeVersion {
     // and set impl_version to 0. If only runtime
     // implementation changes and behavior does not, then leave spec_version as
     // is and increment impl_version.
-    spec_version: 68,
+    spec_version: 69,
     impl_version: 0,
     apis: RUNTIME_API_VERSIONS,
-    transaction_version: 21,
+    transaction_version: 22,
     state_version: 1,
 };
 
